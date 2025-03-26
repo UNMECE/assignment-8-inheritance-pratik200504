e05ff639c1eab9233e37549380b7d63a01d50082
pratik200504 <pojha@unm.edu>
2025-03-26 15:02:58 -0600

    just for the hint

---
+++ main.cpp
@@ -0,0 +1,35 @@
+include<iostream>
+#include<cmath>
+
+using namespace std;
+
+const double EPSILON_0=8.854e-12; //permitivity of free space
+const double MU_0=4* M_PI* 1e-7; //permeability of free space
+
+class Field
+{
+	private *value;
+	public:
+	//default constructor
+	//parameterized
+	//deconstructor
+void printMagnitude();
+
+
+class Electric_field:public Field{
+private E;
+public:
+//default construcor
+//parametrized
+//deco
+//
+
+}
+void calEF(doubler, doubleQ)
+{
+	E=Q/4*M_PI*pow(r.2)*EPSILON_0;
+
+
+	}
+
+
